03c3e0fda422416d70bce9f8c29e330d446a94da
mohamedmoamen <mohamed.moamen.mohamed@gmail.com>
2017-12-20 11:09:49 +0200

    sample

--- app/src/main/java/com/bluehomestudio/quickoperationdemo/HelpActivity.java
+++ app/src/main/java/com/bluehomestudio/quickoperationdemo/HelpActivity.java
@@ -13,6 +13,7 @@ public class HelpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
     }
 
 }
